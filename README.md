822f2fa23e042079e19faf04ccccd8a9e189f6ca
KenWhite02 <whiteken2002@gmail.com>
2020-12-31 12:20:11 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+## Project Title
+
+Covid-19 Tracker <br/>
+
+As cases are still on the rise, as a programmer I built a Covid tracker to keep you up to date on the latest cases.
+
+## Screenshot
+
+![rona1](https://user-images.githubusercontent.com/68158625/103403538-91811980-4b61-11eb-83c5-97151df6b3ae.png)
+
+## Technologies Used
+
+React, Material UI & React Leaflet.
+
+## View Project
+
+You can view the current project at [https://rona-19-tracker.netlify.app/](https://rona-19-tracker.netlify.app/)
+
+## Installation
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+### License
+
+MIT © KenWhite.
---
+++ src/App.js
@@ -0,0 +1,156 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import logo from './assets/covid.png';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from '@material-ui/core';
+import InfoBox from './InfoBox';
+import LineGraph from './LineGraph';
+import Table from './Table';
+import { sortData, prettyPrintStat } from './util';
+import numeral from 'numeral';
+import Map from './Map';
+import 'leaflet/dist/leaflet.css';
+
+const App = () => {
+  const [country, setInputCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+  const [countries, setCountries] = useState([]);
+  const [mapCountries, setMapCountries] = useState([]);
+  const [tableData, setTableData] = useState([]);
+  const [casesType, setCasesType] = useState('cases');
+  const [mapCenter, setMapCenter] = useState({ lat: 1, lng: 38 });
+  const [mapZoom, setMapZoom] = useState(3);
+
+  useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/all')
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      fetch('https://disease.sh/v3/covid-19/countries')
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          let sortedData = sortData(data);
+          setCountries(countries);
+          setMapCountries(data);
+          setTableData(sortedData);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
+  console.log(casesType);
+
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
+
+    const url =
+      countryCode === 'worldwide'
+        ? 'https://disease.sh/v3/covid-19/all'
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setInputCountry(countryCode);
+        setCountryInfo(data);
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(3);
+      });
+  };
+
+  return (
+    <div className='app'>
+      <div className='app__left'>
+        <div className='app__header1'>
+          <h1>
+            C
+            <span>
+              <img src={logo} alt='logo' width='40' />
+            </span>
+            VID-19 Tracker
+          </h1>
+        </div>
+        <div className='app__stats'>
+          <InfoBox
+            onClick={(e) => setCasesType('cases')}
+            title='Cases'
+            isRed
+            active={casesType === 'cases'}
+            cases={prettyPrintStat(countryInfo.todayCases)}
+            total={numeral(countryInfo.cases).format('0.0a')}
+          />
+          <InfoBox
+            onClick={(e) => setCasesType('recovered')}
+            title='Recovered'
+            active={casesType === 'recovered'}
+            cases={prettyPrintStat(countryInfo.todayRecovered)}
+            total={numeral(countryInfo.recovered).format('0.0a')}
+          />
+          <InfoBox
+            onClick={(e) => setCasesType('deaths')}
+            title='Deaths'
+            isRed
+            active={casesType === 'deaths'}
+            cases={prettyPrintStat(countryInfo.todayDeaths)}
+            total={numeral(countryInfo.deaths).format('0.0a')}
+          />
+        </div>
+        <div className='dropdown'>
+          <FormControl className='app__dropdown'>
+            <Select
+              variant='outlined'
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value='worldwide'>Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <Map
+          countries={mapCountries}
+          casesType={casesType}
+          center={mapCenter}
+          zoom={mapZoom}
+        />
+      </div>
+      <Card className='app__right'>
+        <CardContent>
+          <div className='app__information'>
+            <div className='app__header2'>
+              <h1>
+                C
+                <span>
+                  <img src={logo} alt='logo' width='40' />
+                </span>
+                VID-19 Tracker
+              </h1>
+            </div>
+            <h3>Live Cases by Country</h3>
+            <Table countries={tableData} />
+            <h3>Worldwide new {casesType}</h3>
+            <LineGraph casesType={casesType} />
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default App;
